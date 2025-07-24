518498f9eea599cc7490368399ecc72392df0d7a
K1111n <kaja_nan@hotmail.de>
2025-07-24 13:39:43 +0200

    feat: Login implemented

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+$username = $_POST['username'] ?? '';
+$password = $_POST['password']  ?? '';
+
+
+if($username == 'test@email.de' && $password == 'testpassword') {
+    $_SESSION['logged_in'] = true;
+    header ("Location: member_area.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Fehler</title>
+    <link rel="stylesheet" href="styles.css">    
+</head>
+<body style="display:flex;flex-direction:column;">
+    <section class="card">
+        <h2>False Username or Password</h2><br>
+        <a href="login.html">Back to Login</a>
+    </section>
+</body>
+</html>
---
+++ member_area.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header('Location: login.html');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Member Area</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <section class="card">
+        <h1>Member Area</h1>
+        <p>Welcome to the Member Area!</p>
+    </section>
+</body>
+</html>
